431635a851c74fe18f5833e931f7d3af79945ab8
Daehee Yoon <allendy11@gmail.com>
2022-02-24 13:57:49 +0900

    virginAus/fix mobileMenu

--- virginAustralia/client/src/components/Nav.js
+++ virginAustralia/client/src/components/Nav.js
@@ -1,17 +1,21 @@
 import React, { useState, useRef } from "react";
 import logo from "images/virgin_logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 const Nav = () => {
   const mobileMenuRef = useRef();
+  const navRef = useRef();
   const menuHandler = () => {
-    mobileMenuRef.current.classList.toggle("mobileMenu_active");
+    // mobileMenuRef.current.classList.toggle("mobileMenu_active");
+    navRef.current.classList.toggle("mobileMenu_active");
   };
 
   return (
     <div className="nav" id="nav">
-      <div className="nav_container">
+      <div className="nav_container" ref={navRef}>
         <div className="navbar">
-          <div className="nav_mobileMenu">
-            <div className="mobileMenu_box" onClick={menuHandler}>
+          <div className="nav_mobileMenu" onClick={menuHandler}>
+            <div className="mobileMenu_box">
               <div className="mobileMenu_icon">
                 <span className="bar"></span>
                 <span className="bar"></span>
@@ -26,19 +30,49 @@ const Nav = () => {
           </div>
         </div>
 
-        <div className="mobileMenu_content" ref={mobileMenuRef}>
+        <div className="mobileMenu_content">
           <div className="content_box">
             <ul className="nav_items">
-              <li>HOME</li>
-              <li>COMPANIES</li>
-              <li>ABOUT US</li>
-              <li>OUR FOUNDATION</li>
-              <li>BRANSON FAMILY</li>
-              <li>VIRGIN RED</li>
+              <li>
+                HOME
+                <div className="icon_RightArrow">
+                  <FontAwesomeIcon icon={faArrowRight} color=" #ce2f22" />
+                </div>
+              </li>
+              <li>
+                COMPANIES{" "}
+                <div className="icon_RightArrow">
+                  <FontAwesomeIcon icon={faArrowRight} color=" #ce2f22" />
+                </div>
+              </li>
+              <li>
+                ABOUT US{" "}
+                <div className="icon_RightArrow">
+                  <FontAwesomeIcon icon={faArrowRight} color=" #ce2f22" />
+                </div>
+              </li>
+              <li>
+                OUR FOUNDATION{" "}
+                <div className="icon_RightArrow">
+                  <FontAwesomeIcon icon={faArrowRight} color=" #ce2f22" />
+                </div>
+              </li>
+              <li>
+                BRANSON FAMILY{" "}
+                <div className="icon_RightArrow">
+                  <FontAwesomeIcon icon={faArrowRight} color=" #ce2f22" />
+                </div>
+              </li>
+              <li>
+                VIRGIN RED{" "}
+                <div className="icon_RightArrow">
+                  <FontAwesomeIcon icon={faArrowRight} color=" #ce2f22" />
+                </div>
+              </li>
             </ul>
           </div>
-          <div className="mobileMenu_content_box"></div>
-          <div className="mobileMenu_content_box"></div>
+          <div className="content_box"></div>
+          <div className="content_box"></div>
         </div>
 
         <div className="navbar">
