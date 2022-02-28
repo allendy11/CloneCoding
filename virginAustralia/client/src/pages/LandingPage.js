97bf42e406e5071b44c3ddb6ffeae554e7198707
Daehee Yoon <allendy11@gmail.com>
2022-02-28 09:27:00 +0900

    virginAus/add section1

--- virginAustralia/client/src/pages/LandingPage.js
+++ virginAustralia/client/src/pages/LandingPage.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import Section1 from 'components/Landingpage/Section1'
 const LandingPage = () => {
-  return <div className="landing"></div>;
+  return <div className="landing">
+    <Section1 />
+  </div>;
 };
 
 export default LandingPage;
