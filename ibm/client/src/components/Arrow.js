c237088e9e297635dfe6526695adeb4bf64f56bf
Daehee Yoon <allendy11@gmail.com>
2022-03-05 22:27:48 +0900

    ibm/add nav

---
+++ ibm/client/src/components/Arrow.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Arrow = () => {
+  return (
+    <div id="arrow">
+      <span className="bar"></span>
+      <span className="bar"></span>
+    </div>
+  );
+};
+
+export default Arrow;
---
+++ ibm/client/src/components/Landingpage/Section5.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Section5 = () => {
+  return (
+    <div id="Section5" className="section">
+      section5
+    </div>
+  );
+};
+
+export default Section5;
