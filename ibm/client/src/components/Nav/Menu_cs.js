399dd19ffeabdc2e8743ef248d46d2d82304dcd5
Daehee Yoon <allendy11@gmail.com>
2022-03-09 21:30:51 +0900

    ibm/update navMenu logic

--- ibm/client/src/components/Nav/Menu_cs.js
+++ ibm/client/src/components/Nav/Menu_cs.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { cs_list } from "components/Nav/menuList";
 const Menu_cs = () => {
   const [listId, setListId] = useState("");
 
@@ -7,16 +7,9 @@ const Menu_cs = () => {
     <div className="navMenu_box">
       <div className="box">
         <ul>
-          <li>IBM Consulting</li>
-          <li>비즈니스 프로세스 서비스</li>
-          <li>디자인 및 비즈니스 전략</li>
-          <li>하이브리드 멀티클라우드 서비스</li>
-          <li>인재 관리 및 혁신</li>
-          <li>애플리케이션 서비스</li>
-          <li>보안 서비스</li>
-          <li>기술 지원 서비스</li>
-          <li>글러벌 파이낸싱</li>
-          <li>모든 서비스 보기</li>
+          {cs_list.map((el, idx) => (
+            <li>{el.list}</li>
+          ))}
         </ul>
       </div>
       <div className="box"></div>
--- ibm/client/src/components/Nav/Menu_ps.js
+++ ibm/client/src/components/Nav/Menu_ps.js
@@ -1,24 +1,14 @@
 import React, { useState } from "react";
+import { ps_list } from "components/Nav/menuList";
 const Menu_ps = () => {
   const [listId, setListId] = useState("");
   return (
     <div className="navMenu_box">
       <div className="box">
         <ul>
-          <li>핵심 솔루션</li>
-          <li>주요 제품 및 플랫폼</li>
-          <li>산업군</li>
-          <li>AI 인공지능</li>
-          <li>자동화</li>
-          <li>블록체인</li>
-          <li>비즈니스 운영</li>
-          <li>클라우드 컴퓨팅</li>
-          <li>데이터/분석</li>
-          <li>IT 인프라 관리</li>
-          <li>보안</li>
-          <li>공급망 관리</li>
-          <li>글로벌 파이낸싱</li>
-          <li>모든 제품 보기</li>
+          {ps_list.map((el) => {
+            return <li>{el.list}</li>;
+          })}
         </ul>
       </div>
       <div className="box"></div>
