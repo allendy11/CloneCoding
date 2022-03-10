b763fb9d847ec8f5c6a4baf60a475b5b842df42c
Daehee Yoon <allendy11@gmail.com>
2022-03-10 22:15:20 +0900

    ibm/fix navMenu

--- ibm/client/src/components/Nav/menuList.js
+++ ibm/client/src/components/Nav/menuList.js
@@ -1,47 +1,129 @@
 export const ps_list = [
   {
     list: "핵심 솔루션",
-    children: [],
+    children: ["하이브리드 클라우드", "AI 인공지능"],
   },
   {
     list: "  주요 제품 및 플랫폼",
-    children: [],
+    children: [
+      "IBM Automation",
+      "IBM Cloud",
+      "IBM Cloud Paks",
+      "IBM FlashSystem",
+      "IBM Maximo Application Suite",
+      "IBM Power",
+      "IBM QRadar",
+      "RedHat",
+      "Watson",
+      "Zero Trust",
+    ],
   },
   {
     list: "산업군",
-    children: [],
+    children: [
+      "뱅킹/금융시장",
+      "에너지/유틸리티",
+      "정부",
+      "의료",
+      "보험",
+      "제조",
+      "소매",
+      "통신",
+      "여행/운송",
+    ],
   },
   {
     list: "자동화",
-    children: [],
+    children: [
+      "고객 서비스",
+      "딥러닝",
+      "원격 모니터링",
+      "플랫폼/API",
+      "모든 AI 제품 보기",
+    ],
   },
   {
     list: "블록체인",
-    children: [],
+    children: [
+      "블록체인 플랫폼",
+      "투명한 공급망 관리",
+      "산업용 블록체인",
+      "무역 금융용 블록체인",
+      "공급망용 블록체인",
+      "식품용 블록체인",
+      "식품용 블록체인",
+      "블록체인 솔루션",
+      "모든 블록체인 제품 보기",
+    ],
   },
   {
     list: "비즈니스 운영",
-    children: [],
+    children: [
+      "엔터프라이즈 자산 관리(EAM)",
+      "시스템 엔지니어링",
+      "IBM 환경 인텔리전스 제품군",
+      "모든 비즈니스 운영 제품 보기",
+    ],
   },
   {
     list: "클라우드 컴퓨팅",
-    children: [],
+    children: [
+      "컨테이너",
+      "데이터베이스",
+      "DevOps",
+      "하이브리드 클라우드",
+      "IBM Cloud Paks",
+      "Red Hat OpenShift",
+      "VMWare",
+      "모든 클라우드 컴퓨팅 제품 보기",
+    ],
   },
   {
     list: "데이터/분석",
-    children: [],
+    children: [
+      "데이터 관리",
+      "비즈니스 인텔리전스",
+      "데이터 패브릭",
+      "데이터 거버넌스",
+      "데이터 사이언스 및 AI",
+      "IBM Cloud Pak for Data",
+      "모든 데이터/분석 제품 보기",
+    ],
   },
   {
     list: "IT 인프라 관리",
-    children: [],
+    children: [
+      "데이터 스토리지",
+      "엔터프라이즈 서버",
+      "하이브리드 클라우드 인프라",
+      "하이브리드 클라우드 인프라",
+      "메인프레임",
+      "모든 IT 인프라 제품 보기",
+      "모든 IT 인프라 제품 보기",
+    ],
   },
   {
     list: "보안",
-    children: [],
+    children: [
+      "데이터 보안",
+      "ID 및 엑세스 관리",
+      "IBM QRadar",
+      "SIEM",
+      "SOAR",
+      "제로 트러스트",
+      "모든 보안 제품 보기",
+    ],
   },
   {
     list: "공급망 관리",
-    children: [],
+    children: [
+      "IBM Sterling",
+      "공급망 가시성",
+      "공급업체 협업",
+      "비즈니스 프로세스 개선",
+      "주문 관리 및 처리",
+      "모든 공급망 제품 보기",
+    ],
   },
   {
     list: "글로벌 파이낸싱",
@@ -55,15 +137,30 @@ export const ps_list = [
 export const cs_list = [
   {
     list: "IBM Consulting",
-    children: [],
+    children: ["전략 컨설팅", "경험 컨설팅", "운영 컨설팅", "기술 컨설팅"],
   },
   {
     list: "비즈니스 프로세스 서비스",
-    children: [],
+    children: [
+      "인공지능 서비스",
+      "데이터 분석 컨설팅",
+      "비즈니스 프로세스 아웃소싱",
+      "엣지 컨설팅",
+      "파이낸스 혁신",
+      "운영 컨설팅",
+      "구매 및 아웃소싱 서비스",
+      "리스크 및 사기 관리 컨설팅",
+      "공급망 관리 컨설팅",
+    ],
   },
   {
     list: "디자인 및 비즈니스 전략",
-    children: [],
+    children: [
+      "디지털 전략",
+      "고객경험 전략",
+      "마케팅 플랫폼 전략",
+      "Salesforce 컨설팅",
+    ],
   },
   {
     list: "하이브리드 멀티클라우드 서비스",
@@ -71,19 +168,25 @@ export const cs_list = [
   },
   {
     list: "인재 관리 및 혁신",
-    children: [],
+    children: ["HR 혁신", "인재 확보", "인재 개발"],
   },
   {
     list: "애플리케이션 서비스",
-    children: [],
+    children: ["애플리케이션 현대화", "엔터프라이즈 애플리케이션 전략"],
   },
   {
     list: "보안 서비스",
-    children: [],
+    children: [
+      "애플리케이션 보안",
+      "데이터 보안",
+      "ID 및 액세스 관리",
+      "매니지드 보안",
+      "보안 거버넌스",
+    ],
   },
   {
     list: "기술 지원 서비스",
-    children: [],
+    children: ["오픈 소스", "서드파티 및 멀티벤더", "IBM 보증 및 유지보수"],
   },
   {
     list: "글러벌 파이낸싱",
@@ -97,44 +200,81 @@ export const cs_list = [
 export const es_list = [
   {
     list: "지원",
-    children: [],
+    children: ["IBM 지원", "IBM Cloud 지원", "IBM 개발자", "IBM 커뮤니티"],
   },
   {
     list: "문서",
-    children: [],
+    children: [
+      "모든 제품 문서",
+      "IBM Cloud의 제품용",
+      "유스 케이스(IBM Redbooks)",
+    ],
   },
   {
     list: "개발자 교육",
-    children: [],
+    children: [
+      "코드 패턴",
+      "개발자 커뮤니티",
+      "개발자 이벤트",
+      "오픈소스@IBM",
+      "기술 아티클",
+      "튜토리얼",
+      "영상",
+      "더 많은 개발자 교육 보기",
+    ],
   },
   {
     list: "교육/훈련",
-    children: [],
+    children: ["교육 과정", "학습 여정", "전문 자격증", "디지털 학습 구독"],
   },
   {
     list: "리소스",
-    children: [],
+    children: ["블로그", "케이스 스터디", "이벤트"],
   },
   {
     list: "알아보기",
-    children: [],
+    children: [
+      "인공지능",
+      "자동화",
+      "블록체인",
+      "비즈니스 인텔리전스",
+      "클라우드 컴퓨팅",
+      "사이버보안",
+      "DevOps",
+      "하이브리드 클라우드",
+      "쿠버네티스",
+      "퀀텀 컴퓨팅",
+      "공급망",
+    ],
   },
 ];
 export const more_list = [
   {
     list: "파트너",
-    children: [],
+    children: [
+      "IBM의 파트너 (PartnerWorld)",
+      "IBM의 전략적 제휴",
+      "IBM 파트너의 지불 플랜",
+    ],
   },
   {
     list: "IBM 리서치",
-    children: [],
+    children: ["리서치 영역", "연구원 디렉토리", "특허", "인재 채용"],
   },
   {
     list: "IBM 정보",
-    children: [],
+    children: [
+      "연간 보고서",
+      "채용",
+      "기업의 사회적 책임",
+      "IR(기업활동)",
+      "새로운 소식 및 공지사항",
+      "사고 리더쉽",
+      "보안, 프라이버시 및 신뢰",
+    ],
   },
   {
     list: "COVID-19",
-    children: [],
+    children: ["비즈니스 솔루션", "행동수칙"],
   },
 ];
