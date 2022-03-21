f4220cc0575b2c446bd4177c0fb137c2b2b69446
Daehee Yoon <allendy11@gmail.com>
2022-03-22 00:14:09 +0900

    interactive_web_react/add scroll event

---
+++ interactice_web/practice3/client/src/pages/Landingpage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Stage from "../components/Landingpage/Stage";
+const Landingpage = () => {
+  return (
+    <div id="Landingpage">
+      <Stage />
+    </div>
+  );
+};
+
+export default Landingpage;
