f516bc44532a1a0f44eba20af444806ad155f824
Daehee Yoon <allendy11@gmail.com>
2022-04-22 23:07:38 +0900

    todo_vue/basic

---
+++ todo_vue/client/src/components/TodoView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="todo-box">
+    <input type="checkbox" />
+    <span class="todo">{{ todo.title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
